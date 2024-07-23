d3068b84602410dc50db9f43f2a0e51a39ccad15
shinnkura <n.shinzo1010@icloud.com>
2024-07-23 18:35:21 +0900

    コメントの追加

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -9,6 +9,15 @@ class Blog extends Model
 {
     use HasFactory;
 
+    /* 
+    laravelでは、「マスアサイメント」という考え方がある
+    フォームやリクエストから送られてきたデータを一気にモデルに登録する方法
+    でも安全のために、Laravelはどのデータをまとめて保存してもいいかを指定する必要があります。
+    これがfillableというものです。fillableに書かれた項目だけがまとめて保存されます。
+
+    まとめて、指定するときにfillableを使う
+    今回、画像は個別で指定したため、fillableにはimageは含めていません。
+     */
     protected $fillable = [
         'title',
         'body',
