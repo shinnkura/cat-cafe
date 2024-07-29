429d3cd690a44f174670d841e46c9a9521161253
shinnkura <n.shinzo1010@icloud.com>
2024-07-29 16:13:21 +0900

    ザ登録の実装

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AdminBlogController;
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
 
 // viewファイルを表示するだけなら、Router::viewメソッドを使うと簡潔に書ける
 // Route::get('/', function () {
@@ -21,3 +22,7 @@ Route::post('/admin/blogs', [AdminBlogController::class, 'store'])->name('admin.
 Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
 Route::put('/admin/blogs/{blog}', [AdminBlogController::class, 'update'])->name('admin.blogs.update');
 Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('admin.blogs.destroy');
+
+// ユーザー管理
+Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
+Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
