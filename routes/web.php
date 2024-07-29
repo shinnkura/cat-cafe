b4c1a53bd3adea2d9ad2d56bda5dab6485fd0623
shinnkura <n.shinzo1010@icloud.com>
2024-07-29 17:56:11 +0900

    ルートグループの作成

--- routes/web.php
+++ routes/web.php
@@ -16,24 +16,35 @@ Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 Route::post('/contact', [ContactController::class, 'sendmail']);
 Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
 
-// ブログ
-Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index')->middleware('auth');
-Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
-Route::post('/admin/blogs', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
-Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
-Route::put('/admin/blogs/{blog}', [AdminBlogController::class, 'update'])->name('admin.blogs.update');
-Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('admin.blogs.destroy');
+// 管理画面
+Route::prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        // ログイン時のみアクセス可能なルート
+        Route::middleware('auth')
+            ->group(function () {
+                // ブログ
+                // Route::get('/blogs', [AdminBlogController::class, 'index'])->name('blogs.index');
+                // Route::get('/blogs/create', [AdminBlogController::class, 'create'])->name('blogs.create');
+                // Route::post('/blogs', [AdminBlogController::class, 'store'])->name('blogs.store');
+                // Route::get('/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('blogs.edit');
+                // Route::put('/blogs/{blog}', [AdminBlogController::class, 'update'])->name('blogs.update');
+                // Route::delete('/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('blogs.destroy');
+                Route::resource('blogs', AdminBlogController::class)->except(['show']);
 
-// ユーザー管理
-Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
-Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
+                // ユーザー管理
+                Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+                Route::post('/users', [UserController::class, 'store'])->name('users.store');
 
-// 認証
-// Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
-// Route::post('/admin/login', [AuthController::class, 'login']);
-Route::prefix('admin')->namespace('Admin')->group(function () {
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('admin.login')->middleware('guest');
-    Route::post('/login', [AuthController::class, 'login']);
-});
+                // ログアウト
+                Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+            });
 
-Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
+        // 未ログイン時のみアクセス可能なルート
+        Route::middleware('guest')
+            ->group(function () {
+                // ログイン
+                Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+                Route::post('/login', [AuthController::class, 'login']);
+            });
+    });
