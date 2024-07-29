5125988f69856a3882eb3f3c0aae0bd23301a8c7
shinnkura <n.shinzo1010@icloud.com>
2024-07-29 16:39:06 +0900

    ログイン処理の追加

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminBlogController;
 use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\AuthController;
 
 // viewファイルを表示するだけなら、Router::viewメソッドを使うと簡潔に書ける
 // Route::get('/', function () {
@@ -26,3 +27,11 @@ Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->n
 // ユーザー管理
 Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
 Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
+
+// 認証
+// Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+// Route::post('/admin/login', [AuthController::class, 'login']);
+Route::prefix('admin')->namespace('Admin')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
